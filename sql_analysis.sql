05dc49ab5227c76c80da69e7ac843906d88ff2c7
Tanisha Payne <9735654+tanisaur@users.noreply.github.com>
2024-09-11 14:34:28 -0400

    Create sql_analysis.sql

---
+++ sql_analysis.sql
@@ -0,0 +1,40 @@
+--Select & From --
+SELECT id , account_id, occurred_at
+FROM orders;
+
+--LIMIT --
+SELECT occurred_at, account_id, channel
+FROM web_events
+LIMIT 15;
+
+--Return the 10 earliest orders in the orders table. 
+--Include the id, occurred_at, and total_amt_usd
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at 
+LIMIT 10;
+
+--Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 5;
+
+--Write a query to return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd asc
+LIMIT 20;
+
+--Write a query that displays the order ID, account ID, and total dollar amount for all the orders, 
+--sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order)
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id ASC, total_amt_usd DESC;
+
+--write a query that again displays order ID, account ID, and total dollar amount for each order,
+--but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC, account_id ASC;
+
